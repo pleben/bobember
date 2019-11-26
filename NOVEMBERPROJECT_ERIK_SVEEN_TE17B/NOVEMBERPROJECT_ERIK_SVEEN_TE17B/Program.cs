a9af1b6de6acea5e2b36d9b8b5cd8767390fe1ea
pleben <55139224+pleben@users.noreply.github.com>
2019-11-26 13:06:34 +0100

    blev klar
    blev klar

--- NOVEMBERPROJECT_ERIK_SVEEN_TE17B/NOVEMBERPROJECT_ERIK_SVEEN_TE17B/Program.cs
+++ NOVEMBERPROJECT_ERIK_SVEEN_TE17B/NOVEMBERPROJECT_ERIK_SVEEN_TE17B/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NOVEMBERPROJECT_ERIK_SVEEN_TE17B
 {
@@ -6,7 +7,55 @@ namespace NOVEMBERPROJECT_ERIK_SVEEN_TE17B
     {
         static void Main(string[] args)
         {
-            
+
+
+
+
+
+            List<Båt> dock = new List<Båt>(); //spara alla båtar man får
+
+
+
+            Båt båtett()     //kallar på constructors i metoder för att skapa nya båtar som är redo för sjösättning
+            {
+                return new Båt(5, 5, "Ett");
+            }
+            Båt båttvå()
+            {
+                return new Båt(5, 10, "två");
+            }
+            Båt båttre()
+            {
+                return new Båt(1, 1, "tre"); // ska vara namn istället för nummer
+            }
+            Båt båtfyra()
+            {
+                return new Båt(1, 1, "fyra");
+            }
+            Båt båtfem()
+            {
+                return new Båt(1, 1, "fem");
+            }
+            Båt båtsex()
+            {
+                return new Båt(1, 1, "sex");
+            }
+            Båt båtsju()
+            {
+                return new Båt(1, 1, "sju");
+            }
+            Båt båtåtta()
+            {
+                return new Båt(1, 1, "åtta");
+            }
+            Båt båtnio()
+            {
+                return new Båt(1, 1, "nio");
+            }
+            Båt båttio()
+            {
+                return new Båt(1, 1, "tio");
+            }
 
 
 
@@ -14,9 +63,147 @@ namespace NOVEMBERPROJECT_ERIK_SVEEN_TE17B
 
 
 
+
+
+
+            Båt lightpool()     //metoden som faktiskt tilllåter mig att få olika båtar när jag bygger från lightpoolen, avgränsar även lightpoolen till vissa båtar only.
+            {
+                Båt nyBåt = null;  // så att jag kan returna båten för det funkade inte om inte nybåt var = null
+
+
+                int Båtbestämmare = Båt.Båtrullare.Next(1, 11);
+
+
+
+                if (Båtbestämmare == 1)       //rullar efter de olika båtarna med hjälp av min static rng-generator, en if-sats/båt som går att få, samma chans att få varje båt
+                {
+                    nyBåt = båtett();
+                }
+                if (Båtbestämmare == 2)
+                {
+                    nyBåt = båttvå();
+                }
+                if (Båtbestämmare == 3)
+                {
+                    nyBåt = båttre();
+                }
+                if (Båtbestämmare == 4)
+                {
+                    nyBåt = båtfyra();
+                }
+                if (Båtbestämmare == 5)
+                {
+                    nyBåt = båtfem();
+                }
+                if (Båtbestämmare == 6)
+                {
+                    nyBåt = båtsex();
+                }
+                if (Båtbestämmare == 7)
+                {
+                    nyBåt = båtsju();
+                }
+                if (Båtbestämmare == 8)
+                {
+                    nyBåt = båtåtta();
+                }
+                if (Båtbestämmare == 9)
+                {
+                    nyBåt = båtnio();
+                }
+                if (Båtbestämmare == 10)
+                {
+                    nyBåt = båttio();
+                }
+
+                return nyBåt;
+
+
+
+
+            }
+
+
+            while (true) //loop så att spelet alltid ska gå igång och inte stängas ned när man har valt ett val och blivit klar med det.
+            {
+
+
+
+
+                string[] items = { " Build Light", " Build Heavy", " Build Special", " Look at dock" }; //en array för att ha alla alternativ som du kan välja mellan i
+                int selected = 0; //ett nummer för att kunna göra saker med valet som spelaren väljer
+                while (true) //en meny
+                {
+
+                    Console.WriteLine("Shikian, what do you want to do?");
+                    Console.WriteLine();
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        if (i == selected) Console.Write(">");
+                        else Console.Write(" ");
+                        Console.WriteLine(items[i]);
+                    }
+
+
+                    ConsoleKeyInfo key = Console.ReadKey(); //få menyn att ha en pil som flyttas
+                    if (key.Key == ConsoleKey.UpArrow) selected--;
+                    if (key.Key == ConsoleKey.DownArrow) selected++;
+                    if (key.Key == ConsoleKey.Enter) break; //klickar du enter så slutar menyn menya så du kan gå vidare
+
+                    Console.Clear();    //hela denna bit ser till så att du inte kan gå under eller ovan de alternativen som finns i menyn
+                    if (selected > items.Length - 1)
+                    {
+                        selected = items.Length - 1;
+                    }
+
+                    if (selected < 0)
+                    {
+                        selected = 0;
+                    }
+                }
+
+                Console.Clear();
+                if (selected == 0)   //om du valde lightpool så visar detta den random båten och säger att den blev addad till din dock
+                {
+                    dock.Add(lightpool());
+                    Console.WriteLine("You got " + dock[dock.Count - 1].name + " added to your dock");
+                }
+
+                if (selected == 1)
+                {
+                    Console.WriteLine("Pool currently under construction, please be patient!");   //inte klar med så går inte att få någonting
+                }
+
+                if (selected == 2)
+                {
+                    Console.WriteLine("Pool currently under construction, please be patient!");
+                }
+
+                if (selected == 3)   //en "meny" där man får alla båtar man hitills har byggt utskriva med sina stats
+                {
+                    for (int i = 0; i < dock.Count; i++)
+                    {
+                        dock[i].printstats();
+                    }
+                }
+
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
 
 
 
         }
+
+
+
+
     }
+
+
+
+
+
 }
---
+++ NOVEMBERPROJECT_ERIK_SVEEN_TE17B/NOVEMBERPROJECT_ERIK_SVEEN_TE17B/båt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NOVEMBERPROJECT_ERIK_SVEEN_TE17B
+{
+    class Båt
+    {
+        public string name;
+        public int hp;
+        public int firepower;
+        public bool alive;
+        //items
+
+        public Båt(int _hp, int _firepower, string _name) //gör båt med stats så att man kan kalla på klassen via metoder i main och skapa dem där
+        {
+            hp = _hp;
+            firepower = _firepower;
+            name = _name;
+        }
+
+        public void hurts(int dmg)    //metoder för om jag skulle hinna med fighting delen av spelet, som nu inte finns med finns grund för
+        {
+            hp -= dmg;
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+
+        public bool alivecheck() //lever? ja/nej
+        {
+            if (hp > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int doDmg()
+        {
+            //math stuff
+            return firepower;
+        }
+
+        public static Random Båtrullare = new Random(); // static så att man inte ska få samma båt varje gång
+
+
+        public void printstats () // skriver ut alla stats med mellanrumm i slutet så att man kan se när det blir en ny båt.
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("HP: " + hp);
+            Console.WriteLine("Firepower: " + firepower);
+            Console.WriteLine();
+        }
+
+
+
+    }
+
+
+}
